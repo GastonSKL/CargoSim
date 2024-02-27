d595cb6a780fbe9e96a9c7d7a08d96a9160198ec
GastonSKL <gastonlskamlec@gmail.com>
2024-02-27 20:16:54 -0300

    user service composition

--- frontend/src/app/Services/users.service.ts
+++ frontend/src/app/Services/users.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private httpClient = inject(HttpClient);
+  private baseUrl:string;
+
+  constructor() { 
+    this.baseUrl = 'https://localhost:44338';
+  }
+
+  register(formValue: any){
+    return firstValueFrom(
+      this.httpClient.post<any>(`${this.baseUrl}/User/Login`, formValue)
+    )
+  }
 }
