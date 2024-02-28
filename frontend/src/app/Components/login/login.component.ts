7f45764b9c3467c6e6f18fa76c5545ee635151c3
GastonSKL <gastonlskamlec@gmail.com>
2024-02-28 18:09:04 -0300

    Adding functionality to the login component, and validations.

--- frontend/src/app/Components/login/login.component.ts
+++ frontend/src/app/Components/login/login.component.ts
@@ -1,28 +1,60 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms'; // Import Validators
 import { UsersService } from '../../Services/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   formulario: FormGroup;
 
   userService = inject(UsersService);
+  router = inject(Router);
+  isLogged: any = undefined;
+  isButtonDisabled: boolean = false;
+  submitted: boolean = false;
 
   constructor() {
     this.formulario = new FormGroup({
-      username: new FormControl(),
-      password: new FormControl()
-    })
+      username: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]), // Add Validators.required
+      password: new FormControl('', [Validators.required]), // Add Validators.required
+    });
   }
 
-  async onSubmit(){
+  async onSubmit() {
     debugger;
+    this.submitted = true;
     localStorage.clear();
-    const response = await this.userService.register(this.formulario.value);
-    localStorage.setItem("token",response.token);
+    try {
+      if (this.formulario.valid) { // Check if the form is valid
+        let response = await this.userService.register(this.formulario.value);
+        localStorage.setItem('token', response.token);
+        if (response.token && response.userName) {
+          this.isLogged = true;
+          setTimeout(() => {
+            this.router.navigate(['/']);
+          }, 1500);
+        } else {
+          this.isLogged = false;
+          this.isButtonDisabled = true;
+          setTimeout(()=>{
+            this.isButtonDisabled = false;
+          }, 1500);
+        }
+      } else {
+        // Form is invalid, handle accordingly
+        console.log('Form is invalid');
+      }
+    } catch (err) {
+      console.log(err);
+      this.isLogged = false;
+      this.isButtonDisabled = true;
+        setTimeout(()=>{
+          this.isButtonDisabled = false;
+        }, 1500);
+    }
   }
 }
